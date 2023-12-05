99ca11e07e9678eac2b3d3880879d50f6e04dff8
DragCk <146335758+DragCk@users.noreply.github.com>
2023-12-06 01:57:32 +0800

    Start client side
    Create react and install all necessary
    Add api modules , configs and Api client

---
+++ client/src/api/client/private.client.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import queryString from "query-string";
+
+const baseURL = "http://localhost:5000/api/v1";
+
+const privateClient = axios.create({
+  baseURL,
+  //對 URL 參數進行自定義的序列化處理
+  paramsSerializer: {
+    //使用 queryString 庫將參數序列化。
+    encode: (params) => queryString.stringify(params),
+  },
+});
+
+//設定 Request 攔截器，它在每次發送請求之前執行。
+privateClient.interceptors.request(async (config) => {
+  return {
+    ...config,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("actkn")}`,
+    },
+  };
+});
+
+//設定了 Response 攔截器，它在每次收到響應之後執行。
+privateClient.interceptors.response.use(
+  (response) => {
+    if (response && response.data) return response.data;
+    return response;
+  },
+  (err) => {
+    throw err.response.data;
+  }
+);
+
+export default privateClient;
---
+++ client/src/api/client/public.client.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import queryString from "query-string";
+
+const baseURL = "http://localhost:5000/api/v1";
+
+const publicClient = axios.create({
+  baseURL,
+  //對 URL 參數進行自定義的序列化處理
+  paramsSerializer: {
+    //使用 queryString 庫將參數序列化。
+    encode: (params) => queryString.stringify(params),
+  },
+});
+
+//設定 Request 攔截器，它在每次發送請求之前執行。
+publicClient.interceptors.request(async (config) => {
+  return {
+    ...config,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+});
+
+//設定了 Response 攔截器，它在每次收到響應之後執行。
+publicClient.interceptors.response.use(
+  (response) => {
+    if (response && response.data) return response.data;
+    return response;
+  },
+  (err) => {
+    throw err.response.data;
+  }
+);
+
+export default publicClient;
