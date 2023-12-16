7be5e222d9106000d505cdb1849784f39a07f16b
DragCk <146335758+DragCk@users.noreply.github.com>
2023-12-17 01:24:41 +0800

    Add media search
    Add media search

---
+++ client/src/components/common/PersonMediaGrid.jsx
@@ -0,0 +1,11 @@
+import { Button, Grid } from "@mui/material";
+import { useEffect, useState } from "react";
+import tmdbConfigs from "../../api/configs/tmdb.configs";
+import personAoi from "../../api/modules/person.api";
+import MediaItem from "./MediaItem";
+
+const PersonMediaGrid = ({ personId }) => {
+  return <div>PersonMediaGrid</div>;
+};
+
+export default PersonMediaGrid;
