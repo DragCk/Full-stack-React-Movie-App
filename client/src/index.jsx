eb2307ed84ef53576ca3597c56933585ff462ebe
DragCk <146335758+DragCk@users.noreply.github.com>
2024-12-12 00:25:32 +0800

    add vercel change

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
