a153c787531faebc783d8f1f4bd7e272b8f227d6
DragCk <146335758+DragCk@users.noreply.github.com>
2024-12-12 19:35:22 +0800

    Fix test

--- server/index.js
+++ server/index.js
@@ -8,7 +8,13 @@ import routes from "./src/routes/index.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [""],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
