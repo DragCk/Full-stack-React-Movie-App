3dfbaa3da38b3bf77945a38f1c5bf869defdf591
DragCk <146335758+DragCk@users.noreply.github.com>
2024-12-12 19:51:52 +0800

    add

--- server/index.js
+++ server/index.js
@@ -8,13 +8,7 @@ import routes from "./src/routes/index.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: ["https://full-stack-react-movie-app.vercel.app/"],
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
